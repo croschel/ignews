a1a544af96d13a016a40fb57918c44e25a00c9d0
Caique Roschel <croschel000@gmail.com>
2021-03-26 17:52:14 -0300

    adjust for load screen with SSG

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,11 @@
 import styles from "./styles.module.scss";
 
-export function SubscribeButton() {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton(props: SubscribeButtonProps) {
+  const { priceId } = props;
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { GetServerSideProps, GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home(props: HomeProps) {
+  const { product } = props;
   return (
     <>
       <Head>
@@ -15,12 +26,57 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>{`for ${product.amount} month`}</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl codding" />
       </main>
     </>
   );
 }
+
+/* export const getServerSideProps: GetServerSideProps = async () => {
+  // const price = await stripe.prices.retrieve("price_1IZLPoKGeTeC6sxDNK2Kj7No", {
+  //  expand: ["product"],
+  // }); 
+  // podemos simplesmente mostrar todos os atributos presentes em prices utilizando expand
+
+  const price = await stripe.prices.retrieve("price_1IZLPoKGeTeC6sxDNK2Kj7No");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+}; */
+
+export const getStaticProps: GetStaticProps = async () => {
+  // const price = await stripe.prices.retrieve("price_1IZLPoKGeTeC6sxDNK2Kj7No", {
+  //  expand: ["product"],
+  // });
+  // podemos simplesmente mostrar todos os atributos presentes em prices utilizando expand
+
+  const price = await stripe.prices.retrieve("price_1IZLPoKGeTeC6sxDNK2Kj7No");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24h
+  };
+};
