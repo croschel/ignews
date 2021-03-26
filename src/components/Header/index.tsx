7c3e9c0e3f9436a44b3dfeee65e8880dcdaa1636
Caique Roschel <croschel000@gmail.com>
2021-03-26 13:20:39 -0300

    working on main inside the _app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -6,9 +7,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ignews" />
         <nav>
-          <a href="">Home</a>
+          <a className={styles.active} href="">
+            Home
+          </a>
           <a href="">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
